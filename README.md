c22526b6072cb17188fafe63cebfc8dbfb139c45
Emanuel Rabina <emanuelrabina@gmail.com>
2019-04-28 11:05:58 +1200

    Extract utilities from common projects

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# dom-utils
-A collection of utilities for working with the DOM
+
+dom-utils
+=========
+
+[![Build Status](https://travis-ci.org/ultraq/dom-utils.svg?branch=master)](https://travis-ci.com/ultraq/dom-utils)
+[![npm](https://img.shields.io/npm/v/@ultraq/dom-utils.svg?maxAge=3600)](https://www.npmjs.com/package/@ultraq/dom-utils)
+[![License](https://img.shields.io/github/license/ultraq/dom-utils.svg?maxAge=2592000)](https://github.com/ultraq/dom-utils/blob/master/LICENSE.txt)
+
+A collection of utilities for working with the DOM.
+
+
+Installation
+------------
+
+```
+npm install @ultraq/dom-utils
+```
+
+
+API
+---
+
+### addEventDelegate(element, eventName, selector, handler)
+
+Add an event listener to `element` that only fires when the target of the event
+matches `selector`.
+
+ - **element**:
+ - **eventName**:
+ - **selector**:
+ - **handler**:
+
+### clearChildren(element)
+
+Removes all of an element's child nodes.
+
+ - **element**
+
+### deserialize(htmlString)
+
+Deserialize an HTML string into a document fragment.
+
+ - **htmlString**
+
+### serialize(documentFragment)
+
+Serialize a document fragment into an HTML string.
+
+ - **documentFragment**
---
+++ dom-utils.js
@@ -0,0 +1,63 @@
+/* 
+ * Copyright 2019, Emanuel Rabina (http://www.ultraq.net.nz/)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Add an event listener to `element` that only fires when the target of the
+ * event matches `selector`.
+ * 
+ * @param {Element} element
+ * @param {String} eventName
+ * @param {String} selector
+ * @param {Function} handler
+ */
+export function addEventDelegate(element, eventName, selector, handler) {
+	element.addEventListener(eventName, event => {
+		if (event.target.matches(selector)) {
+			handler(event);
+		}
+	});
+}
+
+/**
+ * Removes all of an element's child nodes.
+ * 
+ * @param {Element} element
+ */
+export function clearChildren(element) {
+	while (element.firstChild) {
+		element.removeChild(element.firstChild);
+	}
+}
+
+/**
+ * Deserialize an HTML string into a document fragment.
+ * 
+ * @param {String} htmlString
+ * @return {DocumentFragment}
+ */
+export function deserialize(htmlString) {
+	return document.createRange().createContextualFragment(htmlString);
+}
+
+/**
+ * Serialize a document fragment into an HTML string.
+ * 
+ * @param {DocumentFragment} documentFragment
+ * @return {String}
+ */
+export function serialize(documentFragment) {
+	return new XMLSerializer().serializeToString(documentFragment);
+}
